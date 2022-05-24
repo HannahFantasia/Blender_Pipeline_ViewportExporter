34510fee2fa1e0d296a39f3986835f7047616aa7
Hannah Ümit <49023943+Koterminion@users.noreply.github.com>
2022-05-24 15:15:38 +0200

    small fix audio

--- demo_exporter.py
+++ demo_exporter.py
@@ -53,8 +53,8 @@ class DemoExport(bpy.types.Operator):
         bpy.context.scene.render.ffmpeg.buffersize = 224 * 8
         bpy.context.scene.render.ffmpeg.packetsize = 2048
         bpy.context.scene.render.ffmpeg.muxrate = 10080000
-        bpy.context.scene.render.ffmpeg.audio_channels = "STEREO"
         bpy.context.scene.render.ffmpeg.audio_codec = "MP3"
+        bpy.context.scene.render.ffmpeg.audio_channels = "STEREO"
         bpy.context.scene.render.ffmpeg.audio_bitrate = 192
         bpy.context.scene.render.ffmpeg.audio_mixrate = 48000
         bpy.ops.render.opengl(animation=True)
